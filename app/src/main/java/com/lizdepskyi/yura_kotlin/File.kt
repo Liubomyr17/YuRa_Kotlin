73b1adab1e95e6bc8e45ff53f9c56fe35030c4c3
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-12 20:26:27 +0300

    259. Змінні

--- app/src/main/java/com/lizdepskyi/yura_kotlin/File.kt
+++ app/src/main/java/com/lizdepskyi/yura_kotlin/File.kt
@@ -1,5 +1,15 @@
 package com.lizdepskyi.yura_kotlin
 
 fun main() {
-println("Hello World")
+    var greeting = "Hello"
+
+    greeting = "Hi!"
+    var number: Int = 1
+
+    println("$greeting My favorite number is $number")
+
+    val name = "Liubomyr"
+    val age = 30
+
+    println("My name is $name and I'm $age years old")
 }
