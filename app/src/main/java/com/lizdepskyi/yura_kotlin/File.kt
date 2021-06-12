93c656153b1a636cb6dc448a5fbbf222513bf410
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-12 23:46:17 +0300

    264. Колекції

--- app/src/main/java/com/lizdepskyi/yura_kotlin/File.kt
+++ app/src/main/java/com/lizdepskyi/yura_kotlin/File.kt
@@ -1,36 +1,22 @@
 package com.lizdepskyi.yura_kotlin
 
-import java.util.*
-
 fun main() {
 
-    /*val names = arrayOf("Anne", "Peter", "Jeff")
-    for (name in names)
-        println(name)
+    /*val set = mutableSetOf("a", "b", "c")
+    set.add("d")
+    for (item in set) println(item) */
 
-    for (x in 0..10) println(x)
-
-    for (x in 0 until 10) println(x)
-
-    for (x in 0 until 10 step 2) println(x)
-
-    for (x in 0 downTo 10) println(x)
-
-    for (x in 0 downTo 10 step 2) println(x)
-
-    val numbers = 10 downTo 0 step 2
-
-    val numberList = numbers.toList()
-    println(numberList)
-    println(numbers)
-
-    for (x in numbers step 2) println(x)*/
-
-    var x = 0
-    while (x < 10) {
-        if (x % 2 == 0) {
-            println(x)
-        }
-        x++
+    val text = "Class Java"
+    val letterSet = mutableSetOf<Char>()
+    var letterCount = 0
+    for (letter in text) {
+        letterSet.add(letter)
+}
+    for (letter in letterSet) {
+        letterCount++
+        println(letter)
     }
+
+    println(letterCount)
+    println(letterSet.size)
 }
