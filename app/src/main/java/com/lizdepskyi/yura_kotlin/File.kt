96b31f57a696daa7bef1f4fd9844ffada84cf7a8
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-13 00:05:58 +0300

    265. Функції

--- app/src/main/java/com/lizdepskyi/yura_kotlin/File.kt
+++ app/src/main/java/com/lizdepskyi/yura_kotlin/File.kt
@@ -2,21 +2,18 @@ package com.lizdepskyi.yura_kotlin
 
 fun main() {
 
-    /*val set = mutableSetOf("a", "b", "c")
-    set.add("d")
-    for (item in set) println(item) */
+    /*fun happyBirthday(name: String, age: Int): String {
+        return "Happy ${age}th birthday, $name!"
+    }*/
 
-    val text = "Class Java"
-    val letterSet = mutableSetOf<Char>()
-    var letterCount = 0
-    for (letter in text) {
-        letterSet.add(letter)
-}
-    for (letter in letterSet) {
-        letterCount++
-        println(letter)
+    fun happyBirthday(name: String, age: Int): String =
+        "Happy ${age}th birthday, $name!"
+
+        println(happyBirthday("Bob", 23))
+
+    fun happyBirthdayWithoutParams() {
+        println("Happy birthday!")
     }
 
-    println(letterCount)
-    println(letterSet.size)
+    happyBirthdayWithoutParams()
 }
